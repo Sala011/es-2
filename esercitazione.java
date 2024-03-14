c371893aeca3a5fb07e20bb1e0379eb4975b4b0e
Sala011 <160124031+Sala011@users.noreply.github.com>
2024-03-14 11:59:59 +0100

    Add files via upload

---
+++ esercitazione.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class esercitazione {
+    public static void main(String[]args);
+    Scanner in = new Scanner(System.in)
+    String {} stringa = new String {100};
+    int scelta
+            System.out.println("1 - Aggiunta di un nome;
+                                2 - Cancellazione del singolo nome;
+                                3 - Ricerca sequenziale di un nome;
+                                4 - Visualizza nomi ripetuti con numero ripetizioni;
+                                5 - Modifica di un nome;
+                                6 - Visualizzazione di tutti i nomi presenti;
+                                7 - Ricerca del nome più lungo e più corto;
+                                8 - Cancellazione di tutte le occorrenze di un nome;
+                                0 - Uscita")
+
+    }
+}
